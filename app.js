fdfee564d82c0a7766ee53e0524ca81d604fe8b2
daliborkozar <daliborkozar@hotmail.com>
2020-02-03 17:06:08 +0100

    note add to array

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ class App {
     constructor() {
         this.$form = document.querySelector('#form')
         this.$noteTitle = document.querySelector('#note-title')
-        this.$formButtons = document.querySelector('#form-buttons');
+        this.$noteText = document.querySelector('#note-text')
+        this.$formButtons = document.querySelector('#form-buttons')
+        this.notes = []
         this.addEventListeners()
         
     }
@@ -11,6 +13,19 @@ class App {
     addEventListeners(){
         document.body.addEventListener('click', event => {
             this.handleFormClick(event)
+        })
+
+        this.$form.addEventListener('submit', event => {
+            event.preventDefault()
+            const title = this.$noteTitle.value
+            const text = this.$noteText.value
+            const hasNote = title || text
+
+            if(hasNote){
+                //addNote
+                this.addNote({ title, text })
+            }
+
         })
     }
 
@@ -25,15 +40,28 @@ class App {
     }
 
     openForm() {
+        this.$form.classList.add('form-open'); 
         this.$noteTitle.style.display = 'block'
         this.$formButtons.style.display = 'block'
     }
 
     closeForm(){
+        this.$form.classList.remove('form-open'); 
         this.$noteTitle.style.display = 'none'
         this.$formButtons.style.display = 'none'
     }
-
+    //add notes to notes array
+    addNote(note){
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white',
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+          };
+          this.notes = [...this.notes, newNote];
+          console.log(this.notes)
+        
+    }
     
 
 
