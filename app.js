7f97c6596646f8c90b8eb068f16c21462206cd67
daliborkozar <daliborkozar@hotmail.com>
2020-02-04 17:27:51 +0100

    display notes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 class App {
     constructor() {
-        this.$form = document.querySelector('#form')
-        this.$noteTitle = document.querySelector('#note-title')
-        this.$noteText = document.querySelector('#note-text')
-        this.$formButtons = document.querySelector('#form-buttons')
-        this.notes = []
-        this.addEventListeners()
+        this.notes = [];
+
+        this.$placeholder = document.querySelector('#placeholder');
+        this.$form = document.querySelector("#form");
+        this.$notes = document.querySelector('#notes');
+        this.$noteTitle = document.querySelector("#note-title");
+        this.$noteText = document.querySelector("#note-text");
+        this.$formButtons = document.querySelector("#form-buttons");
+        
+
+
+        this.addEventListeners();
         
     }
 
@@ -16,9 +22,12 @@ class App {
         })
 
         this.$form.addEventListener('submit', event => {
+            //prevent default da ne dodje do reload koji je po defaultu
             event.preventDefault()
+            // grabimo unos
             const title = this.$noteTitle.value
             const text = this.$noteText.value
+            //ako je jedan od uslova true
             const hasNote = title || text
 
             if(hasNote){
@@ -27,6 +36,9 @@ class App {
             }
 
         })
+
+
+
     }
 
     handleFormClick(event){
@@ -56,14 +68,40 @@ class App {
             title: note.title,
             text: note.text,
             color: 'white',
+            // da bi imalo razlicit id za svaki novi note
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
           };
           this.notes = [...this.notes, newNote];
-          console.log(this.notes)
+
+          this.displayNotes()
+
+
         
     }
     
+    //displays notes to #placehoder 
+    displayNotes(){
+        const hasNotes = this.notes.length > 0;    
+     if (hasNotes) {
+       this.$placeholder.style.display = 'none';  //
+     } else {
+       this.$placeholder.style.display = 'flex';    
+     }
 
+     this.$notes.innerHTML = this.notes.map(note => `
+     <div style="background: ${note.color};" class="note">
+       <div class="${note.title && 'note-title'}">${note.title}</div>
+       <div class="note-text">${note.text}</div>
+       <div class="toolbar-container">
+         <div class="toolbar">
+           <img class="toolbar-color" src="https://icon.now.sh/palette">
+           <img class="toolbar-delete" src="https://icon.now.sh/delete">
+         </div>
+       </div>
+     </div>
+  `);
+
+    }
 
 }
 
